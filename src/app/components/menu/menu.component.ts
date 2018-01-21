4a4a16a7cd7b3ae5417db356c034221c3891d5fe
Dixita Ganatra <dixita0607@gmail.com>
2018-01-21 23:16:04 +0530

    Initial deployment

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -6,13 +6,20 @@ import {Router} from "@angular/router";
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('books')
+  books: ElementRef;
 
   constructor(private router: Router) {
   }
 
   ngOnInit() {
     this.router.navigate(['books']);
+  }
+
+  ngAfterViewInit() {
+    this.books.nativeElement.focus();
   }
 
 }
